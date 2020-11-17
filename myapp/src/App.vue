960f25fff5076d1554be95c0e1cd68d078c1e4eb
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-17 15:39:48 +0100

    added navbar component

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-   
+   <navbar/>
   </div>
 </template>
 
 <script>
-
+import Navbar from './components/navigation/Navbar'
 
 export default {
   name: 'App',
   components: {
-    
+    Navbar,
   }
 }
 </script>
