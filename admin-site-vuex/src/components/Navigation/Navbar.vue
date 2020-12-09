a987b9d660a50de3ebf08cd59cec6f69a2be69d6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-09 16:52:48 +0100

    added navbar w routerlinks

---
+++ admin-site-vuex/src/components/Navigation/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark primary-color">
+  <router-link to="/" class="navbar-brand">Admin-site</router-link>
+  <div class="ml-auto">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <router-link to="/" class="nav-link" exact>Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/signIn" class="nav-link" exact>SignIn</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/users" class="nav-link" exact>Users</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
