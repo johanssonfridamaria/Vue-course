c81bdbba5ff4b431fa25db6933c39d1a8982482b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-09 16:52:23 +0100

    importet navbar component

--- admin-site-vuex/src/App.vue
+++ admin-site-vuex/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar/>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from './components/Navigation/Navbar'
+
+export default {
+  name: 'App',
+  components: { Navbar, },
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
