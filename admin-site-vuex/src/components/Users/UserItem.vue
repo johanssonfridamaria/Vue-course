98a21fbdda8d738b5bc361bdcb7e9da353d2b38b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-11 15:58:57 +0100

    added delete user

--- admin-site-vuex/src/components/Users/UserItem.vue
+++ admin-site-vuex/src/components/Users/UserItem.vue
@@ -3,13 +3,18 @@
       <th scope="row">{{ user.id}}</th>
       <td>{{ user.email}}</td>
       <td>{{ user.name}}</td>
+      <td><button class="btn btn-danger btn-sm" @click="deleteUser(user.id)">X</button></td>
     </tr>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   name: 'Useritem',
-  props:['user']
+  props:['user'],
+  methods: {
+    ...mapActions(['deleteUser']),
+  }
 }
 </script>
 
