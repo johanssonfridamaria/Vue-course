03307af461be940f78b1cfe1b0bde7fc3574d682
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-11 15:57:50 +0100

    added user component

--- admin-site-vuex/src/components/Users/UsersTable.vue
+++ admin-site-vuex/src/components/Users/UsersTable.vue
@@ -1,33 +1,33 @@
 <template>
-<table class="table">
-  <thead class="grey lighten-2">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Email</th>
-      <th scope="col">Name</th>
-    </tr>
-  </thead>
-  <tbody>
- <userItem v-for="user in this.users" :key="user.id" :user="user"/>
-  </tbody>
-
-</table>
+  <table class="table">
+    <thead class="grey lighten-2">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Email</th>
+        <th scope="col">Name</th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <userItem v-for="user in this.users" :key="user.id" :user="user" />
+    </tbody>
+  </table>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import UserItem from './UserItem'
+import { mapGetters } from 'vuex';
+import UserItem from './UserItem';
 
 export default {
   name: 'UsersTable',
-components: {UserItem,},
-computed: {
-  ...mapGetters(['users']),
-}
-}
-
+  components: {
+    UserItem,
+  },
+  computed: {
+    ...mapGetters(['users']),
+  },
+};
 </script>
 
 <style>
-
 </style>
