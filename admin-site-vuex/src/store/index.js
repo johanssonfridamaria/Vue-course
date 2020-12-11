b1077b2da3f8d29842be7c510f0576ba6834c633
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-11 15:59:10 +0100

    imported modules

--- admin-site-vuex/src/store/index.js
+++ admin-site-vuex/src/store/index.js
@@ -1,56 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import users from './modules/users'
+import adminUser from './modules/adminUser'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    users: [
-      {
-        id: 1,
-        email: 'kenth@mail.se',
-        name: 'Kenth',
-        password: 'losen123', 
-        loggedIn: false
-      },
-      {
-        id: 2,
-        email: 'pia@mail.se',
-        name: 'Pia',
-        password: 'losen123', 
-        loggedIn: false
-      },
-      {
-        id: 3,
-        email: 'klara@mail.se',
-        name: 'Klara',
-        password: 'losen123', 
-        loggedIn: false
-      },
-      {
-        id: 4,
-        email: 'arvid@mail.se',
-        name: 'Arvid',
-        password: 'losen123', 
-        loggedIn: false
-      },
-    ],
-    adminUser: { email: 'frida@mail.se', password: 'losen123', loggedIn: false},
-    email:'',
-    password: '',
-    loggedIn: '',
-
-  },
-  getters: {
-    adminUser: state => state.adminUser,
-    users: state => state.users
-  },
-  mutations: {
-    
-  },
-  actions: {
-
-  },
   modules: {
+    users,
+    adminUser,
   }
 })
