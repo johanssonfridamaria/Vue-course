90ffc9514d7d96ce6100f270d610e0728bfaadc0
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-11 15:58:47 +0100

    added method adduser

--- admin-site-vuex/src/components/Users/AddUser.vue
+++ admin-site-vuex/src/components/Users/AddUser.vue
@@ -1,20 +1,45 @@
 <template>
-  <form class="p-5 text-left">
+  <form class="p-5 text-left" @submit.prevent="onSubmit">
     <p class="h4 mb-4 text-left">Add user</p>
-    <label for="email">Email</label>
-    <input type="text" id="email" class="form-control mb-4">
-    <label for="name">Name</label>
-    <input type="text" id="name" class="form-control mb-4">
+    <label for="userEmail">Email</label>
+    <input
+      type="text"
+      id="userEmail"
+      class="form-control mb-4"
+      v-model="user.email"
+    />
+    <label for="userName">Name</label>
+    <input
+      type="text"
+      id="userName"
+      class="form-control mb-4"
+      v-model="user.name"
+    />
     <button class="btn btn-info btn-block my-4" type="submit">Add</button>
-</form>
+  </form>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-
-}
+  data() {
+    return {
+      user: {
+        email: '',
+        name: '',
+      },
+    };
+  },
+  methods: {
+    ...mapActions(['addUser']),
+    onSubmit() {
+      this.addUser(this.user);
+      this.email = '';
+      this.name = '';
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
